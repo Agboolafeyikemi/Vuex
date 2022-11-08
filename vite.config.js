46106c476dcf0ff020298519f6345365d1ebd703
Agboolafeyikemi <agboolafeyikemi93@gmail.com>
2022-11-08 07:34:36 +0000

    change config

--- vite.config.js
+++ vite.config.js
@@ -12,25 +12,4 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/lib.ts"),
-      name: "my-ui-lib",
-      fileName: "my-ui-lib",
-    },
-    rollupOptions: {
-      external: ["vue"],
-      output: [
-        {
-          format: "es",
-          exports: "named",
-          globals: { vue: "vue" },
-        },
-      ],
-    },
-    commonjsOptions: {
-      esmExternals: true,
-    },
-  },
 });
