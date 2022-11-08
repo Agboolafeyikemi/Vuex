912e8b064fae5adf367197e9cd6dcfae8578f773
Agboolafeyikemi <agboolafeyikemi93@gmail.com>
2022-11-08 07:43:04 +0000

    change config

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import { createStore } from "vuex";
 import App from "./App.vue";
-import router from "./router";
+import router from "./router/index";
 
 import "./assets/main.css";
 const store = createStore({
